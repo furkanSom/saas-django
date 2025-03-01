16082a90cf45b9cbd6e0d7684bb1ee1791e12471
Furkan Somuncuoglu <furkansomuncuoglu@gmail.com>
2025-03-01 15:20:16 +0300

    prepare static files

--- src/myapp/settings.py
+++ src/myapp/settings.py
@@ -138,6 +138,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for py manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+# OUTPUT for py manage.py collectstatic
+# local cdn
+
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
