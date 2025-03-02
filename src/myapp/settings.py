fa2ca14d90a2c9500b5a35715f473d4477499c69
Furkan Somuncuoglu <furkansomuncuoglu@gmail.com>
2025-03-02 11:59:52 +0300

    dockerfile edit

--- src/myapp/settings.py
+++ src/myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY") or "j2z-2px^@-+6@twd73jlb-!^&$1m!-)s8q1iht5!987$15@!$*"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool) 
